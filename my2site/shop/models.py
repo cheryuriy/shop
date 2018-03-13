cedae64cf5a45624a7754aee4df1cd853a448f30
cheryuriy <30784719+cheryuriy@users.noreply.github.com>
2018-03-13 11:39:08 +0700

    edition

--- my2site/shop/models.py
+++ my2site/shop/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 from django.utils import timezone
 
-#python manage.py runserver
 
+# Tовары относятся к разным категориям.
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-# Tовары относятся к разным категориям.
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    description = models.CharField(max_length=4000, default='')
-    quantity = models.IntegerField(default=1)
-    incoming_date = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.name
-
-# It's not using for now:
+# Поставщики:
 class Provider(models.Model):
-    product = models.ManyToManyField(Product)
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=24)  #  IntegerField(default=0)
     email = models.EmailField(default = None)
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=4000, default='')
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField(default=0)
+    incoming_date = models.DateTimeField(default=timezone.now)
+    providers = models.ManyToManyField(Provider)
+
+    def __str__(self):
+        return self.name
+
+
+
 
--- my2site/shop/views.py
+++ my2site/shop/views.py
@@ -7,17 +7,31 @@ import json
 def my_response_404(clas, obj_id):
     error1 = {"status": "404"}
     error1["source"] = {"pointer": "/data/" + str(clas) + "/obj_id"}
-    error1["detail"] = 'Object With Id = ' + str(obj_id) + ' Does Not Exist.'
+    error1["detail"] = 'Object With Identifier = ' + str(obj_id) + ' Does Not Exist.'
     out = {"errors": [error1]}
     return HttpResponseNotFound(json.dumps(out), content_type="application/json")
 
 
 def wrap_products(list_p):
+    if not list_p:
+        return {"data": []}
     done = []
     for prod in list_p:
-        done.append({"name": prod.name,
+        done.append({"name": prod.name, "id": prod.id, "price": prod.price,
                      "incoming_date": prod.incoming_date.strftime('%Y.%m.%d-%H:%M:%S')})
     return {"category" : prod.category.name, "data": done}
+
+
+
+class IndexView(View):
+    model = Category
+
+    def get(self, request):
+        list_c = Category.objects.order_by('name')
+        done = []
+        for cat in list_c:
+            done.append({"name": cat.name, "id": cat.id})
+        return JsonResponse({"data": done})
 
 
 class DetailView(View):
@@ -28,7 +42,10 @@ class DetailView(View):
             obj = Product.objects.get(pk = product_id)
         except Product.DoesNotExist:
             return my_response_404(Product, product_id)
-        return JsonResponse(wrap_products([obj]))
+        wrap_obj = wrap_products([obj])
+        if obj.description != '':
+            wrap_obj["data"][0]["description"] = obj.description
+        return JsonResponse(wrap_obj)
 
 
 class CategoryView(View):
@@ -46,7 +63,8 @@ class CategoryView(View):
         error1 = {"status":"400"}
         if request.content_type != "application/json":
             error1["source"] = {"pointer": "/post/content_type/application_json"}
-            error1["detail"] = 'Wrong POST attempt. "Content-Type: application/json".'
+            error1["title"] = 'Wrong POST attempt.'
+            error1["detail"] = "Content-Type: application/json."
             out = {"errors": [error1]}
             return HttpResponseBadRequest(content = json.dumps(out), content_type = "application/json")
         try:
@@ -54,34 +72,49 @@ class CategoryView(View):
             body = json.loads(body_unicode)
             err = 'name'
             product_name = body['name']
-            # err = 'description'
-            # description = body['description']
+            err = 'price'
+            price = int(body['price'])
+            err = 'provider'
+            provider_name = body['provider']
+            err = 'description'
+            description = body['description']
         except (KeyError):
-            error1["source"] = {"pointer": "/data/" + err}
-            error1["detail"] = 'Wrong POST attempt. Product "name" must be present.'
-            out = {"errors": [error1]}
-            return HttpResponseBadRequest(content=json.dumps(out), content_type="application/json")
+            if err == 'description':
+                description = ''  #  ignoring description
+            else:
+                error1["source"] = {"pointer": "/data/" + err}
+                error1["title"] = "Wrong POST attempt. Product's name, provider and price must be present."
+                error1["detail"] = "No field " + err
+                out = {"errors": [error1]}
+                return HttpResponseBadRequest(content=json.dumps(out), content_type="application/json")
         try:
             cat = Category.objects.get(pk=category_id)
         except Category.DoesNotExist:
             return my_response_404(Category, category_id)
-        s = Product.objects.create(name=product_name, category=cat)  # description = description
-        wr = wrap_products([s])
+        try:
+            provider = Provider.objects.get(name = provider_name)
+        except Provider.DoesNotExist:
+            return my_response_404(Provider, provider_name)
+        p = Product.objects.create(name=product_name, category=cat, price=price, description = description)
+        p.providers.add(provider)
+        wr = wrap_products([p])
+        wr["data"][0]["provider"] = provider_name
         # wr["data"][0]["saved"] = 'Ok'
         return JsonResponse(wr)
 
 
-# curl -i -H "Content-Type: application/json" -X POST -d '{"name": "1_Post"}' http://127.0.0.1:8000/shop/1/
+# curl -i -H "Content-Type: application/json" -X POST -d '{"name": "1_Post", "provider": "pro1", "price": "150"}' http://127.0.0.1:8000/shop/1/
+# curl -i -H "Content-Type: application/json" -X POST -d '{"name": "2_Post","description": "This is description.","provider": "pro2", "price": "250"}' http://127.0.0.1:8000/shop/2/
 
-class IndexView(View):
-    model = Category
-
-    def get(self, request):
-        list_c = Category.objects.order_by('name')
-        done = []
-        for cat in list_c:
-            done.append({"name": cat.name, "id": cat.id})
-        return JsonResponse({"data": done})
-
+# Secret info of providers shouldn't being seen:
+# class ProviderView(View):
+#     model = Provider
+#
+#     def get(self, request):
+#         list_p = Provider.objects.order_by('name')
+#         done = []
+#         for p in list_p:
+#             done.append({"name": p.name, "phone": p.phone, })
+#         return JsonResponse({"data": done})
 
 
